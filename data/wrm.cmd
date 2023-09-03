ce2ebb29818cb58563e6130bd66ab81c4ce1a673
jolly.exe <95768367+xXJollyHAKERXx@users.noreply.github.com>
2023-09-04 01:01:15 +0200

    Update wrm.cmd

--- data/wrm.cmd
+++ data/wrm.cmd
@@ -2,37 +2,37 @@
 title 
 set var1=%1
 
-if exist "%appdata%\windows-registry-mods\wrm.cmd" ( goto wrm-exists ) else ( goto installation )
+if exist "C:\\windows-registry-mods\wrm.cmd" ( goto wrm-exists ) else ( goto installation )
 goto barrier
 
 :installation
-curl -s https://raw.githubusercontent.com/xXJollyHAKERXx/wrm/main/data/wrm.zip -o %temp%\wrm.zip
+curl -s https://raw.githubusercontent.com/jolly-exe/wrm/main/data/wrm.zip -o %temp%\wrm.zip
 cls
-if not exist "%appdata%\windows-registry-mods" ( md %appdata%\windows-registry-mods )
+if not exist "C:\\windows-registry-mods" ( md C:\\windows-registry-mods )
 
-powershell Expand-Archive -Path "%temp%\wrm.zip" -DestinationPath "%appdata%\windows-registry-mods"
-curl -s https://raw.githubusercontent.com/xXJollyHAKERXx/wrm/main/data/latest-version -o %appdata%\windows-registry-mods\data\version.txt
+powershell Expand-Archive -Path "%temp%\wrm.zip" -DestinationPath "C:\\windows-registry-mods"
+curl -s https://raw.githubusercontent.com/jolly-exe/wrm/main/data/latest-version -o C:\\windows-registry-mods\data\version.txt
 del /q /f %temp%\wrm.zip
 goto wrm-exists
 
 goto barrier
 
 :wrm-exists
-curl -s https://raw.githubusercontent.com/xXJollyHAKERXx/wrm/main/data/latest-version -o %appdata%\windows-registry-mods\data\latest-version.txt
+curl -s https://raw.githubusercontent.com/jolly-exe/wrm/main/data/latest-version -o C:\\windows-registry-mods\data\latest-version.txt
 cls
-set /p version= < %appdata%\windows-registry-mods\data\version.txt
-set /p latest-version= < %appdata%\windows-registry-mods\data\latest-version.txt
+set /p version= < C:\\windows-registry-mods\data\version.txt
+set /p latest-version= < C:\\windows-registry-mods\data\latest-version.txt
 if "%version%" == "%latest-version%" ( goto launch ) else ( goto update )
 
 :launch
-start "" "cmd" /c %appdata%\windows-registry-mods\wrm.cmd
+start "" "cmd" /c C:\\windows-registry-mods\wrm.cmd
 exit
 
 goto barrier
 
 :update
 
-rd /s /q "%appdata%\windows-registry-mods"
+rd /s /q "C:\\windows-registry-mods"
 
 goto installation
 
